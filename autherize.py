4ef097e3111c6df7accbb7df7ba7a9fafed87be0
AsbjornL <a.bjorn.2nd@gmail.com>
2024-07-28 12:28:25 +0200

    Properly escape auth url

--- autherize.py
+++ autherize.py
@@ -1,14 +1,21 @@
-from requests.utils import quote
+from urllib.parse import quote
 import conf
 import webbrowser
 
 
+
 def auth_url(redirect_url):
-    url = "https://accounts.spotify.com/authorize"
-    url += "?response_type=code"
-    url += f"&client_id={quote(conf.client_id)}"
-    url += f"&scope={quote(conf.scope)}"
-    url += f"&redirect_uri={quote(redirect_url)}"
+    base_url = "https://accounts.spotify.com/authorize"
+    response_type = "code"
+    client_id = conf.client_id
+    scope = conf.scope
+    
+    url = f"{base_url}?"
+    url += f"response_type={quote(response_type)}"
+    url += f"&client_id={quote(client_id)}"
+    url += f"&scope={quote(scope)}"
+    url += f"&redirect_uri={quote(redirect_url, safe='')}"
+    
     return url
 
 
